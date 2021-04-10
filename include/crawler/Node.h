a696f43ef62f9f33c22579b522492bd8ec13e579
Nikola Gjakovik <nikolag@umich.edu>
2021-04-10 18:46:13 -0500

    big moves

--- include/crawler/Node.h
+++ include/crawler/Node.h
@@ -1,3 +1,9 @@
+
+#pragma once
+
+#ifndef NODE_H_AS
+#define NODE_H_AS 
+
 #include "../../libraries/AS/include/AS/vector.h"
 #include "../../libraries/AS/include/AS/string.h"
 #include "../../libraries/AS/include/AS/Socket.h"
@@ -6,20 +12,74 @@
 #include "../../libraries/AS/include/AS/File.h"
 #include "../../Parser/HtmlParser.h"
 #include "../../libraries/HashTable/include/HashTable/FNV.h"
+#include "../../libraries/AS/include/AS/pthread_pool.h"
+#include "../../libraries/AS/include/AS/circular_buffer.h"
+#include "../../libraries/bloomFilter/include/bloomFilter/BloomFilter.h"
+#include "SetOfUrls.h"
+#include "Database.h"
+
+   template< typename T>
+    class dynamicBuffer : public APESEARCH::Buffer<T>
+       {
+       APESEARCH::vector<T> buffer;
+       typedef T value_type;
+    public:
+       // For default construct objects...
+       dynamicBuffer( const size_t capacity ) : buffer( capacity )
+          {
+          assert( computeTwosPowCeiling( capacity ) == capacity );
+          }
+
+       dynamicBuffer( const size_t capacity, T&val ) : buffer( capacity, val )
+          {
+          assert( computeTwosPowCeiling( capacity ) == capacity );
+          }
+       inline value_type *getBuffer() noexcept
+          {
+          return &buffer.front();
+          }
+       inline void insert( const T& val, size_t index ) noexcept
+          {
+          buffer[ index ] = val;
+          }
+       inline void insert( size_t index, T&& val ) noexcept
+          {
+          buffer[ index ] = std::forward<T>( val );
+          }
+       inline virtual T& get( size_t index )
+          {
+          return buffer[ index ];
+          }
+       
+       inline virtual size_t getCapacity() const
+          {
+          return buffer.size();
+          }
+       inline virtual T *begin() noexcept
+          {
+          return &buffer.front();
+          }
+       void print( std::ostream& os, const size_t , const size_t sizeOf ) const
+          {
+          return;
+          }
+       };
 
 class Node
 {
-private:
-    APESEARCH::unique_ptr<Socket> server;
-    APESEARCH::vector<struct sockaddr_in> addrinfos;
-    APESEARCH::vector<APESEARCH::unique_ptr<Socket>> sockets;
-    APESEARCH::vector<APESEARCH::File> storage_files;
-    APESEARCH::vector<APESEARCH::mutex> locks; //Used for sends, and atomic updates to storage_files
-    APESEARCH::vector<APESEARCH::string> ips;
-    APESEARCH::string local_ip;
-    int node_id;
-    FNV hash;
-
+   using CircBuf = APESEARCH::circular_buffer< APESEARCH::Func, dynamicBuffer< APESEARCH::Func > >;
+   APESEARCH::vector<struct sockaddr_in> addrinfos;
+   APESEARCH::vector<APESEARCH::unique_ptr<Socket>> sockets;
+   APESEARCH::vector<APESEARCH::File> storage_files;
+   APESEARCH::vector<APESEARCH::mutex> locks; //Used for sends, and atomic updates to storage_files
+   APESEARCH::string local_ip;
+   int node_id;
+   FNV hash;
+   SetOfUrls& set;
+   Bloomfilter bloomFilter;
+   APESEARCH::mutex bloomFilter_lock;
+   APESEARCH::PThreadPool< CircBuf > pool;
+   Database& dataBase; 
     
 
 public:
@@ -27,7 +87,7 @@ public:
     //Start listening server
     //Check if swap files exist and how much data they have in them currently
     //Must have ips in some ordering!
-    Node(APESEARCH::vector<APESEARCH::string> &ips, APESEARCH::string &loc_ip);
+    Node(APESEARCH::vector<APESEARCH::string> &ips, APESEARCH::string &loc_ip, SetOfUrls&, Database &db);
     ~Node();
 
     //1 dedicated thread-blocking
@@ -40,6 +100,9 @@ public:
     void send(Link &link);
 
     // 7 dedicated threads non-blocking
-    void receive();
+    void receive(int i);
+
+    void connector(int i);
 };
 
+#endif
---
+++ src/Database.cpp
@@ -0,0 +1,57 @@
+#include "../include/crawler/Database.h"
+#include <stdlib.h>
+
+#define NUM_OF_FILES 256
+
+Database::Database()
+{
+    APESEARCH::string anchor_root = "./anchorFiles/anchorFile";
+    APESEARCH::string parsed_root = "./parsedFiles/parsedFile";
+    char path[PATH_MAX];
+    for(int i = 0; i < NUM_OF_FILES; i++)
+    {   
+        try
+        {
+            snprintf( path, sizeof( path ), "%s%d%s", anchor_root.cstr(), i, ".txt" );
+            anchorFiles.emplace_back( path, O_RDWR | O_CREAT | O_APPEND , (mode_t) 0600 );
+            snprintf( path, sizeof( path ), "%s%d%s", parsed_root.cstr(), i, ".txt" );
+            parsedFiles.emplace_back( path, O_RDWR | O_CREAT | O_APPEND , (mode_t) 0600 );
+        }
+        catch(...)
+        {
+            exit(1);
+        }
+    }
+}
+Database::~Database()
+{
+}
+
+void Database::addAnchorFile(Link &link){
+    if(link.anchorText.empty())
+    {
+        return;
+    }
+    static const char* const null_char = "\0";
+    static const char* const newline_char = "\n";
+    static const char* const space_char = " ";
+
+    int index = hash(link.URL.cstr());
+    index = index % anchorFiles.size();
+
+    {
+        APESEARCH::unique_lock<APESEARCH::mutex> lk( database_locks[index] );
+        anchorFiles[index].write(link.URL.cstr(), link.URL.size());
+        anchorFiles[index].write(newline_char, 1);
+        for(int i = 0; i < link.anchorText.size(); i++){
+            anchorFiles[index].write(link.anchorText[i].cstr(), link.anchorText[i].size());
+            anchorFiles[index].write(space_char, 1);
+        }
+        anchorFiles[index].write(null_char, 1);
+    }
+}
+
+void Database::addParsedFile()
+{
+    
+}
