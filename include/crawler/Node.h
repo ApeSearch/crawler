c45042f6403ae19ccc080c80c55b1edd14a3ba68
Nikola Gjakovik <nikolag@umich.edu>
2021-04-09 23:34:17 -0500

    Node update

--- include/crawler/Node.h
+++ include/crawler/Node.h
@@ -5,16 +5,17 @@
 #include "../../libraries/AS/include/AS/mutex.h"
 #include "../../libraries/AS/include/AS/File.h"
 #include "../../Parser/HtmlParser.h"
-#include "../libraries/AS/include/AS/FNV.h"
+#include "../../libraries/HashTable/include/HashTable/FNV.h"
 
 class Node
 {
 private:
     APESEARCH::unique_ptr<Socket> server;
-    APESEARCH::vector<struct addrinfo> addrinfos; 
-    APESEARCH::vector<unique_ptr<Socket>> sockets;
-    APESEARCH::vector<File> storage_files;
+    APESEARCH::vector<struct addrinfo> addrinfos;
+    APESEARCH::vector<APESEARCH::unique_ptr<Socket>> sockets;
+    APESEARCH::vector<APESEARCH::File> storage_files;
     APESEARCH::vector<APESEARCH::mutex> locks; //Used for sends, and atomic updates to storage_files
+    APESEARCH::vector<APESEARCH::string> ips;
     APESEARCH::string local_ip;
     int node_id;
     FNV hash;
@@ -36,7 +37,7 @@ public:
     //Try to send n times 
     //If cannot send write to local file
     //
-    send(Link &link);
+    void send(Link &link);
 
     // 7 dedicated threads non-blocking
     void receive();
--- src/Node.cpp
+++ src/Node.cpp
@@ -1,16 +1,18 @@
 #include "../include/crawler/Node.h"
-
-
 #include <iostream>
+#include <netinet/in.h>
+#include <sys/types.h>
+
+
 using APESEARCH::unique_ptr;
 
 #define PORT 8080
 
-Node::Node(vector<string> ips, string loc_ip) : sockets(ips.size()), addrinfos(ips.size()), locks(ips.size()), local_ip(loc_ip)
+Node::Node(APESEARCH::vector<APESEARCH::string> ips, APESEARCH::string loc_ip) : sockets(ips.size()), addrinfos(ips.size()), locks(ips.size()), local_ip(loc_ip)
 {
-    constexpr char *pathname = "./storage_files/storage_filei.txt"
+    constexpr char *pathname = "./storage_files/storage_filei.txt";
     //Get handles to files
-    for(int i = 0; i < ip_addresses.size(); ++i)
+    for(int i = 0; i < ips.size(); ++i)
     {
         //TODO decide on pathnames 
         pathname[28] = '0' + i;
@@ -18,9 +20,9 @@ Node::Node(vector<string> ips, string loc_ip) : sockets(ips.size()), addrinfos(i
         try
             { 
             //Make sure that we never have more than one process appending unto these files.
-            storage_files.emplace_back(File(pathname, O_RDWR | O_CREAT | O_APPEND , (mode_t) 0600));
+            storage_files.emplace_back( pathname, O_RDWR | O_CREAT | O_APPEND , (mode_t) 0600 );
             }
-        catch(failure &fail)
+        catch(APESEARCH::File::failure &fail)
             {
             //If you fail here we got nothing to do except manual debug
             std::cerr << "Failed opening node storage file " << pathname << '\n';
@@ -30,13 +32,13 @@ Node::Node(vector<string> ips, string loc_ip) : sockets(ips.size()), addrinfos(i
     }
 
     //Create addrinfo structures for connecting
-    for(int i = 0; i < ip_addresses.size(); ++i)
+    for(int i = 0; i < ips.size(); ++i)
     {
         struct addrinfo &addr = addrinfos[i];
         memset(&(addr), 0, sizeof(addr));
         addr.sin_family = AF_INET;    /* select internet protocol */
         addr.sin_port = htons(PORT);         /* set the port # */
-        addr.sin_addr.s_addr = inet_addr(ips[i].c_str()) /* set the addr */
+        addr.sin_addr.s_addr = inet_addr(ips[i].cstr()) /* set the addr */
 
         if(ips[i] == local_ip)
             node_id = i;
@@ -44,11 +46,11 @@ Node::Node(vector<string> ips, string loc_ip) : sockets(ips.size()), addrinfos(i
 
 
     //Premptive to connect to other nodes that have larger ids
-    for(int i = node_id + 1; i < ip_addresses.size(); ++i)
+    for(int i = node_id + 1; i < ips.size(); ++i)
         {
         try
             {
-            Socket *sock = new Socket(addrinfos[i]);
+            Socket *sock = new Socket((sockaddr_in)addrinfos[i]);
             unique_ptr<Socket> ptr(sock);
             sockets[i].swap(ptr);
             }
@@ -67,6 +69,7 @@ Node::Node(vector<string> ips, string loc_ip) : sockets(ips.size()), addrinfos(i
     catch(...)
     {
         std::cerr << "Error in starting up listening socket" << '\n';
+        exit(1);
     }
     
     //Call thread with ConnectionHandler
@@ -96,24 +99,23 @@ void Node::connectionHandler()
         struct sockaddr_in node_addr;
         socklen_t node_len = sizeof(node_addr);
         
+        bool found_ip = false;
+
         try
         {
-            bool found_ip = false;
-            unique_ptr<Socket> ptr = server.accept((struct sockaddr *) &node_addr, &node_len);
+            unique_ptr<Socket> ptr = server->accept((struct sockaddr *) &node_addr, &node_len);
             char *ip = inet_ntoa(node_addr.sin_addr);
 
             //TODO strcmp vs make a string operator=
 
-            for (int i = node_id - 1; 0 < i; --i)
+            for (int i = node_id - 1; 0 <= i; --i)
             {
                 if(!strcmp(ip, ips[i].cstr()))
                     {
-                    APESEARCH::unique_mutex<APESEARCH::mutex> lk( locks[i] );
+                    APESEARCH::unique_lock<APESEARCH::mutex> lk( locks[i] );
                     sockets[i].swap(ptr);
-
-                    locks[i].unlock();
-
                     found_ip = true;
+                    break;
                 }
             }
             // If someone with a higher node id tries to connect to me write to std::cer
@@ -134,14 +136,14 @@ void Node::connectionHandler()
 //Try to send n times unless the connection is closed,
 //Then try to create a new socket
 //If sent n times and connection is still open or new connection cannot be made write to swap file
-Node::send( Link &link )
+void Node::send( Link &link )
 {
     static const char* const null_char = "\0";
     static const char* const newline_char = "\n";
 
     //TODO switch to modulo operation after testing, since we will have 8 nodes.
-    size_t val = hash(link.URL.c_str);
-    int val = val % addrinfos.size();
+    size_t val = hash(link.URL.cstr());
+    val = val % addrinfos.size();
 
     if(val == node_id)
     {
@@ -153,13 +155,13 @@ Node::send( Link &link )
     else
     {
         
-        if(sockets[val].fd > 0)
+        if( sockets[val]->fd() > 0 )
         {
             try
             {
                 
             }
-            catch()
+            catch(...)
             {
                 
             }
