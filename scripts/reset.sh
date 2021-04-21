82c35480c612c0c69c24b956af2194f9b19333a3
xiongrob <62448777+xiongrob@users.noreply.github.com>
2021-04-21 03:39:34 -0400

    Fix reset.sh so it resets number of websites crawled

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,7 +3,7 @@ rm anchorFiles/*
 rm storageFiles/*
 rm parsedFiles/*
 rm VirtualFileSystem/Root/Frontier/*
-cp VirtualFileSystem/Root/pagesCrawledDONTTOUCH.txt /dev/null
+cat /dev/null > VirtualFileSystem/Root/pagesCrawledDONTTOUCH.txt
 chmod -x scripts/reset.sh
 echo "Please don't rerun again unless you want to remove everything crawled/parsed/indexed"
 rm core
